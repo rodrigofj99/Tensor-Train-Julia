9906764d41707476aa6359aff461f81d0eaec880
Mi-Song <misong.dupuy@gmail.com>
2025-08-26 19:21:41 +0200

    examples

---
+++ examples/1d_homogenisation.jl
@@ -0,0 +1,37 @@
+using TensorTrains
+using Plots
+
+function homogenisation_1d_tto(δ,d)
+  Llow = toeplitz_to_qtto(1,0,-1,d)
+  Lup = toeplitz_to_qtto(1,-1,0,d)
+  h = 1/(2^d+1)
+  ap = 2/3*qtt_polynom([1,1],d;a=h,b=1.0-h)*(1.0+qtt_cos(d;a=h,b=1.0-h,λ=1/δ)*qtt_cos(d;a=h,b=1.0-h,λ=1/δ))
+  am =  2/3*qtt_polynom([1,1],d;a=0,b=1.0-2h)*(1.0+qtt_cos(d;a=0,b=1.0-2h,λ=1/δ)*qtt_cos(d;a=0,b=1.0-2h,λ=1/δ))
+  A_tto = 1/h*Llow*potential(ap)+1/h*Lup*potential(am)
+  overlap = h*toeplitz_to_qtto(4,1,1,d)
+  f_rhs = overlap*ones_tt(2,d)
+  return A_tto, tt_rounding(f_rhs)
+end
+
+"""
+Sanity check
+A_tto, f_tt = homogenisation_1d_tto(0.1,6)
+A6 = qtto_to_matrix(A_tto)
+f6 = qtt_to_function(f_tt)
+u6 = A6\f6
+A_tto, f_tt = homogenisation_1d_tto(0.1,8)
+A8 = qtto_to_matrix(A_tto)
+f8 = qtt_to_function(f_tt)
+u8 = A8\f8
+plot(range(1/2^6,1-1/2^6,length=2^6),u6,label="d=6")
+plot!(range(1/2^8,1-1/2^8,length=2^8),u8,label="d=8")
+"""
+
+A_tto, f_tt = homogenisation_1d_tto(0.1,10)
+dmrg_schedule = dmrg_schedule_default(;N=2,rmax=16,nsweeps=2,it_solver=true)
+u_dmrg = dmrg_linsolv(A_tto,f_tt,deepcopy(f_tt),schedule=dmrg_schedule)
+#u_gmres = tt_gmres(A_tto,f_tt,u_dmrg;Imax=100,tol=1e-8,m=20,hist=false,rmax=256)
+α_opt = 1/(2500*4+0.05/4) #works only for δ=0.1
+
+@time x_tt, res_ϵ = gradient_fixed_step(A_tto,f_tt,α_opt,Imax=50,i_trunc=1,eps_tt=1e-4,rand_rounding=false,r_tt=16,ℓ=8)
+@time x_rr_tt, res_rr_ϵ = gradient_fixed_step(A_tto,f_tt,α_opt,Imax=50,i_trunc=1,eps_tt=1e-4,rand_rounding=true,r_tt=16,ℓ=8)
--- examples/laplacian.jl
+++ examples/laplacian.jl
@@ -1,5 +1,6 @@
 using TensorTrains
 using LinearAlgebra
+using Plots
 
 """
 test run
@@ -25,19 +26,48 @@ const r = 2
 const b2_tt = rand_tt(ntuple(x->n,d),vcat(1,r*ones(Int64,d-1),1),normalise=true)
 const H2_mpo = Δ_tto(n,d)
 
-const Imax = 100
+const Imax = 50
 const α_opt = 2/(d*eigmax(Δ(n))+d*eigmin(Δ(n)))
 const i_trunc = 1
-eps_list = [1e-2]
-#const eps_list = [1e-2,1e-3,1e-4,1e-5,1e-6]
+#eps_list = [1e-2]
+eps_list = [1e-2,1e-3,1e-4]
 const res_list = zeros(length(eps_list),Imax+1)
 const sol_hsvd = Array{Array{Array{Float64,1}}}(undef,length(eps_list))
 
 for i_ε in eachindex(eps_list)
-  x_ϵ_tt, res_ϵ = gradient_fixed_step(H2_mpo,b2_tt,α_opt,Imax=Imax,i_trunc=1,eps_tt=eps_list[i_ε])
+  @time x_ϵ_tt, res_ϵ = gradient_fixed_step(H2_mpo,b2_tt,α_opt,Imax=Imax,i_trunc=i_trunc,eps_tt=eps_list[i_ε],r_tt=50)
   println(x_ϵ_tt.ttv_rks)
   Σ = tt_svdvals(x_ϵ_tt)
   sol_hsvd[i_ε] = Σ
   res_list[i_ε,1] = eps_list[i_ε]
-  res_list[i_ε,2:length(res_ϵ)+1] = res_ϵ/sqrt(n^d)
+  res_list[i_ε,2:length(res_ϵ)+1] = res_ϵ
 end
+
+scatter(res_list[1,2:end][res_list[1,2:end].>0],label="$(eps_list[1])",yscale=:log10)
+scatter!(res_list[2,2:end][res_list[2,2:end].>0],label="$(eps_list[2])",yscale=:log10)
+scatter!(res_list[3,2:end][res_list[3,2:end].>0],label="$(eps_list[3])",yscale=:log10)
+
+"""
+Randomised version
+"""
+
+const res_rand_list = zeros(length(eps_list),Imax+1)
+const sol_rand_hsvd = Array{Array{Array{Float64,1}}}(undef,length(eps_list))
+
+for i_ε in eachindex(eps_list)
+  @time x_ϵ_tt, res_ϵ = gradient_fixed_step(H2_mpo,b2_tt,α_opt,Imax=Imax,i_trunc=i_trunc,eps_tt=eps_list[i_ε],rand_rounding=true,r_tt=50,ℓ=10)
+  println(x_ϵ_tt.ttv_rks)
+  Σ = tt_svdvals(x_ϵ_tt)
+  sol_rand_hsvd[i_ε] = Σ
+  res_rand_list[i_ε,1] = eps_list[i_ε]
+  res_rand_list[i_ε,2:length(res_ϵ)+1] = res_ϵ
+end
+
+# influence of the TT truncation parameter
+scatter(res_rand_list[1,2:end][res_rand_list[1,2:end].>0],label="$(eps_list[1])",yscale=:log10)
+scatter!(res_rand_list[2,2:end][res_rand_list[2,2:end].>0],label="$(eps_list[2])",yscale=:log10)
+scatter!(res_rand_list[3,2:end][res_rand_list[3,2:end].>0],label="$(eps_list[3])",yscale=:log10)
+
+# Comparison deterministic vs randomised rounding
+scatter(res_rand_list[2,2:end][res_rand_list[2,2:end].>0],label="ε=$(eps_list[2]), RandRound",yscale=:log10)
+scatter!(res_list[2,2:end][res_list[2,2:end].>0],label="ε=$(eps_list[2]), HSVD",yscale=:log10)
